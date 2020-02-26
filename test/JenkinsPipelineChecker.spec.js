dc9a16935332cdf3d3136b06b1e8d2f43f0c0410
Chris Martin <chrsmrtn@outlook.com>
2020-02-25 22:11:41 -0700

    adjusted unit test to avoid eventlistener memory leak

--- test/JenkinsPipelineChecker.spec.js
+++ test/JenkinsPipelineChecker.spec.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const JenkinsPipelineChecker = require('../index').JenkinsPipelineChecker;
+const { JenkinsPipelineChecker } = require('../index');
 
 const testConfiguration = {
     jenkinsUrl: 'jenkinsServer',
@@ -15,94 +15,79 @@ const testConfiguration = {
 };
 
 describe('JenkinsPipelineChecker', function () {
+    let jenkinsApiResponse = { building: false, result: 'SUCCESS', url: 'url to pipeline' };
+    let sut;
     describe('#run()', () => {
+        before(function () {
+            sut = buildApp(jenkinsApiResponse);
+        });
+
         it('should return the appropriate signal color for a successful pipeline', async () => {
-            return buildApp( { building: false, result: 'SUCCESS' }).then(async sut => {
-                return sut.run().then((signal) => {
-                    assert.equal(signal.points[0][0].color, testConfiguration.successColor, "Did not get the expected successful color.");
-                });
-            });
+            let signal = await sut.run();
+            assert.equal(signal.points[0][0].color, testConfiguration.successColor, "Did not get the expected successful color.");
         });
 
         it('should return the appropriate signal effect for a failed pipeline', async () => {
-            return buildApp( { building: false, result: 'SUCCESS' }).then(async sut => {
-                return sut.run().then((signal) => {
-                    assert.equal(signal.points[0][0].effect, testConfiguration.successEffect, "Did not get the expected successful effect");
-                });
-            });
+            let signal = await sut.run();
+            assert.equal(signal.points[0][0].effect, testConfiguration.successEffect, "Did not get the expected successful effect");
         });
 
         it('should return the appropriate signal message for a successful pipeline', async () => {
-            return buildApp( { building: false, result: 'SUCCESS' }).then(async sut => {
-                return sut.run().then((signal) => {
-                    assert.ok(signal.message.includes('passed'), "Message didn't indicate if the pipeline passed");
-                    assert.ok(signal.message.includes(testConfiguration.pipeline), "Message didn't include pipeline name");
-                });
-            });
+            let signal = await sut.run();
+            assert.ok(signal.message.includes('passed'), "Message didn't indicate if the pipeline passed");
+            assert.ok(signal.message.includes(testConfiguration.pipeline), "Message didn't include pipeline name");
         });
 
         it('should return the appropriate signal color for a failed pipeline', async () => {
-            return buildApp( { building: false, result: 'FAILURE' }).then(async sut => {
-                return sut.run().then((signal) => {
-                    assert.equal(signal.points[0][0].color, testConfiguration.failureColor, "Did not get the expected failure color.");
-                });
-            });
+            jenkinsApiResponse.result = 'FAILURE';
+            let signal = await sut.run();
+            assert.equal(signal.points[0][0].color, testConfiguration.failureColor, "Did not get the expected failure color.");
         });
 
         it('should return the appropriate signal effect for a failed pipeline', async () => {
-            return buildApp( { building: false, result: 'FAILURE' }).then(async sut => {
-                return sut.run().then((signal) => {
-                    assert.equal(signal.points[0][0].effect, testConfiguration.failureEffect, "Did not get the expected failure effect");
-                });
-            });
+            jenkinsApiResponse.result = 'FAILURE';
+            let signal = await sut.run();
+            assert.equal(signal.points[0][0].effect, testConfiguration.failureEffect, "Did not get the expected failure effect");
         });
 
         it('should return the appropriate signal message for a failed pipeline', async () => {
-            return buildApp( { building: false, result: 'FAILURE' }).then(async sut => {
-                return sut.run().then((signal) => {
-                    assert.ok(signal.message.includes('failed'), "Message didn't indicate if the pipeline failed");
-                    assert.ok(signal.message.includes(testConfiguration.pipeline), "Message didn't include pipeline name");
-                });
-            });
+            jenkinsApiResponse.result = 'FAILURE';
+            let signal = await sut.run();
+            assert.ok(signal.message.includes('failed'), "Message didn't indicate if the pipeline failed");
+            assert.ok(signal.message.includes(testConfiguration.pipeline), "Message didn't include pipeline name");
         });
 
         it('should return the appropriate signal color for a building pipeline', async () => {
-            return buildApp( { building: true, result: null }).then(async sut => {
-                return sut.run().then((signal) => {
-                    assert.equal(signal.points[0][0].color, testConfiguration.buildingColor, "Did not get the expected building color.");
-                });
-            });
+            jenkinsApiResponse.building = true;
+            jenkinsApiResponse.result = null;
+            let signal = await sut.run();
+            assert.equal(signal.points[0][0].color, testConfiguration.buildingColor, "Did not get the expected building color.");
         });
 
         it('should return the appropriate signal effect for a building pipeline', async () => {
-            return buildApp( { building: true, result: null }).then(async sut => {
-                return sut.run().then((signal) => {
-                    assert.equal(signal.points[0][0].effect, testConfiguration.failureEffect, "Did not get the expected building effect");
-                });
-            });
+            jenkinsApiResponse.building = true;
+            jenkinsApiResponse.result = null;
+            let signal = await sut.run();
+            assert.equal(signal.points[0][0].effect, testConfiguration.failureEffect, "Did not get the expected building effect");
         });
 
         it('should return the appropriate signal message for a building pipeline', async () => {
-            return buildApp( { building: true, result: null }).then(async sut => {
-                return sut.run().then((signal) => {
-                    assert.ok(signal.message.includes('building'), "Message didn't indicate if the pipeline is building");
-                    assert.ok(signal.message.includes(testConfiguration.pipeline), "Message didn't include pipeline name");
-                });
-            });
+            jenkinsApiResponse.building = true;
+            jenkinsApiResponse.result = null;
+            let signal = await sut.run();
+            assert.ok(signal.message.includes('building'), "Message didn't indicate if the pipeline is building");
+            assert.ok(signal.message.includes(testConfiguration.pipeline), "Message didn't include pipeline name");
         });
 
         it('should return the appropriate signal link for a pipeline', async () => {
-            return buildApp( { building: false, result: 'SUCCESS', url: 'url to pipeline' }).then(async sut => {
-                return sut.run().then((signal) => {
-                    assert.equal(signal.link.url, 'url to pipeline', "Did not get the expected successful effect");
-                });
-            });
+            let signal = await sut.run();
+            assert.equal(signal.link.url, 'url to pipeline', "Did not get the expected successful effect");
         });
     });
 });
 
-async function buildApp(response, rejection) {
-    let fakeRequest = { get:  async function() {
+function buildApp(response, rejection) {
+    let fakeRequest = { get:  function() {
         return buildResponsePromise(response, rejection);
     }}
     let app = new JenkinsPipelineChecker(fakeRequest);
@@ -111,7 +96,7 @@ async function buildApp(response, rejection) {
     return app;
 }
 
-async function buildResponsePromise(response, rejection){
+function buildResponsePromise(response, rejection){
     let responsePromise;
     if (response) {
         responsePromise = new Promise((resolve, reject) => {
