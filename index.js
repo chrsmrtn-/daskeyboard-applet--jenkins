3cd740465f72a8edc529b9a68eca748e68affac4
Chris Martin <chrsmrtn@outlook.com>
2020-02-28 21:18:40 -0700

    added eslint

--- index.js
+++ index.js
@@ -1,190 +1,186 @@
 const request = require('request-promise');
-const { LastBuild }  = require('./lastBuild');
-
 // Library to send signal to Q keyboards
 const q = require('daskeyboard-applet');
-const logger = q.logger;
+const { LastBuild } = require('./lastBuild');
+
+const { logger } = q;
 
 
-class JenkinsPipelineChecker extends q.DesktopApp
-{
-    constructor(requestParam = request) {
-        super();
-        this.pollingInterval = 3000;
-        this._request = requestParam;
-        
-        this.jenkinsUrl;
-        this.userToken;
+class JenkinsPipelineChecker extends q.DesktopApp {
+  constructor(requestParam = request) {
+    super();
+    this.pollingInterval = 3000;
+    this.request = requestParam;
 
-        this.MessagesForBuild = {
-            SUCCESS: " passed!",
-            FAILURE: " failed!",
-            BUILDING: " building...",
-            ABORTED: " aborted."
-        }
+    this.jenkinsUrl = undefined;
+    this.userToken = undefined;
+
+    this.MessagesForBuild = {
+      SUCCESS: ' passed!',
+      FAILURE: ' failed!',
+      BUILDING: ' building...',
+      ABORTED: ' aborted.',
+    };
+  }
+
+  async applyConfig() {
+    this.ColorsForBuild = {
+      SUCCESS: this.config.successColor,
+      FAILURE: this.config.failureColor,
+      BUILDING: this.config.buildingColor,
+      ABORTED: this.config.abortedColor,
+    };
+
+    this.EffectsForBuild = {
+      SUCCESS: this.config.successEffect,
+      FAILURE: this.config.failureEffect,
+      BUILDING: this.config.buildingEffect,
+      ABORTED: this.config.abortedEffect,
+    };
+
+    try {
+      this.parseApiKeyValue(this.authorization.apiKey);
+    } catch (ex) {
+      logger.error(ex.message);
+    }
+  }
+
+  async run() {
+    const url = `${this.jenkinsUrl}/job/${this.config.pipeline}/lastBuild/api/json`;
+
+    return this.request.get({
+      url,
+      headers: {
+        Authorization: `Basic ${Buffer.from(`${this.userToken}`).toString('base64')}`,
+      },
+      json: true,
+    }).then((body) => {
+      const lastBuild = new LastBuild(body);
+      return this.getSignal(lastBuild);
+    });
+  }
+
+  async options(fieldId) {
+    if (fieldId === 'pipeline') {
+      const url = `${this.jenkinsUrl}/api/json`;
+
+      return this.request.get({
+        url,
+        headers: {
+          Authorization: `Basic ${Buffer.from(`${this.userToken}`).toString('base64')}`,
+        },
+        json: true,
+      }).then((body) => JenkinsPipelineChecker.buildJobs(body.jobs)).catch((error) => {
+        logger.error(`Caught error when loading jobs: ${error}`);
+        return [];
+      });
     }
 
-    async applyConfig() {
-        this.ColorsForBuild = {
-            SUCCESS: this.config.successColor,
-            FAILURE: this.config.failureColor,
-            BUILDING: this.config.buildingColor,
-            ABORTED: this.config.abortedColor
-        }
+    return [];
+  }
 
-        this.EffectsForBuild = {
-            SUCCESS: this.config.successEffect,
-            FAILURE: this.config.failureEffect,
-            BUILDING: this.config.buildingEffect,
-            ABORTED: this.config.abortedEffect
-        }
+  getColor(lastBuild) {
+    let color;
+    const lastBuildStatus = JenkinsPipelineChecker.convertToStatus(lastBuild);
 
-        try {
-            this.parseApiKeyValue(this.authorization.apiKey);
-        } catch (ex) {
-            logger.error(ex.message);
-        }
-        
+    if (Object.keys(this.ColorsForBuild).includes(lastBuildStatus)) {
+      color = this.ColorsForBuild[lastBuildStatus];
+    } else {
+      color = `Build state of ${lastBuild.result} not recognized`;
     }
 
-    async run() {
-        let url = this.jenkinsUrl + '/job/' + this.config.pipeline + '/lastBuild/api/json';
+    return color;
+  }
 
-        return this._request.get({
-            url: url,
-            headers: {
-                'Authorization': 'Basic ' + Buffer.from(`${this.userToken}`).toString('base64')
-            },
-            json: true
-        }).then((body) => {
-            let lastBuild = new LastBuild(body);
-            return this.getSignal(lastBuild);
-        });
+  getEffect(lastBuild) {
+    let effect;
+    const lastBuildStatus = JenkinsPipelineChecker.convertToStatus(lastBuild);
+
+    if (Object.keys(this.EffectsForBuild).includes(lastBuildStatus)) {
+      effect = this.EffectsForBuild[lastBuildStatus];
+    } else {
+      effect = `Build state of ${lastBuild.result} not recognized`;
     }
 
-    async options(fieldId) {
-        if (fieldId === 'pipeline') {
-            let url = this.jenkinsUrl + '/api/json';
-    
-            return this._request.get({
-                url: url,
-                headers: {
-                    'Authorization': 'Basic ' + Buffer.from(`${this.userToken}`).toString('base64')
-                },
-                json: true
-            }).then(body => {
-                return this.buildJobs(body.jobs);
-            }).catch(error => {
-                logger.error(`Caught error when loading jobs: ${error}`);
-                return [ ];
-            });
-        }
+    return effect;
+  }
+
+  getStatusMessage(lastBuild) {
+    let status;
+    const lastBuildStatus = JenkinsPipelineChecker.convertToStatus(lastBuild);
+
+    if (Object.keys(this.MessagesForBuild).includes(lastBuildStatus)) {
+      status = this.MessagesForBuild[lastBuildStatus];
+    } else {
+      status = `Build state of ${lastBuild.result} not recognized`;
     }
 
-    getColor(lastBuild, config)
-    {
-        let color;
-        let lastBuildStatus = this.convertToStatus(lastBuild);
+    return status;
+  }
 
-        if (Object.keys(this.ColorsForBuild).includes(lastBuildStatus)) {
-            color = this.ColorsForBuild[lastBuildStatus];
-        } else {
-            color = `Build state of ${lastBuild.result} not recognized`;
-        }
+  getSignal(lastBuild) {
+    const color = this.getColor(lastBuild, this.config);
+    const effect = this.getEffect(lastBuild, this.config);
+    let message = this.config.pipeline;
+    message += this.getStatusMessage(lastBuild);
 
-        return color;
+    const signal = new q.Signal({
+      points: [[new q.Point(color, effect)]],
+      name: 'Jenkins',
+      message,
+      link: {
+        url: lastBuild.url,
+        label: 'Show in Jenkins',
+      },
+    });
+
+    return signal;
+  }
+
+
+  static convertToStatus(lastBuild) {
+    let lastBuildStatus = lastBuild.result;
+    if (lastBuild.building) {
+      lastBuildStatus = 'BUILDING';
+    }
+    return lastBuildStatus;
+  }
+
+  static buildJobs(jobs) {
+    const options = [];
+    jobs.forEach((job) => {
+      options.push({
+        key: job.name,
+        value: job.name,
+      });
+    });
+    return options;
+  }
+
+  parseApiKeyValue(apiKey) {
+    if (apiKey === undefined) {
+      return;
     }
 
-    getEffect(lastBuild, config)
-    {
-        let effect;
-        let lastBuildStatus = this.convertToStatus(lastBuild);
-
-        if (Object.keys(this.EffectsForBuild).includes(lastBuildStatus)) {
-            effect = this.EffectsForBuild[lastBuildStatus];
-        } else {
-            effect = `Build state of ${lastBuild.result} not recognized`;
-        }
-
-        return effect;
+    if (!apiKey.startsWith('http')) {
+      throw new Error('Unable to parse connection string. Connection needs to use protocol of "http" or "https".');
     }
 
-    getStatusMessage(lastBuild)
-    {
-        let status;
-        let lastBuildStatus = this.convertToStatus(lastBuild);
-
-        if (Object.keys(this.MessagesForBuild).includes(lastBuildStatus)) {
-            status = this.MessagesForBuild[lastBuildStatus];
-        } else {
-            status = `Build state of ${lastBuild.result} not recognized`;
-        }
-
-        return status;
+    const urlParts = apiKey.split('//');
+    const protocol = urlParts[0];
+    if (!urlParts[1].includes('@')) {
+      throw new Error('Unable to parse connection string. Missing @ separator.');
     }
 
-    getSignal(lastBuild) {
-        let color = this.getColor(lastBuild, this.config);
-        let effect = this.getEffect(lastBuild, this.config);
-        let message = this.config.pipeline;
-        message += this.getStatusMessage(lastBuild);
-
-        let signal = new q.Signal({
-            points: [[new q.Point(color, effect)]],
-            name: "Jenkins",
-            message: message,
-            link: {
-                url: lastBuild.url,
-                label: 'Show in Jenkins',
-            }
-        });
-
-        return signal;
-    }
-
-    convertToStatus(lastBuild) {
-        let lastBuildStatus = lastBuild.result;
-        if (lastBuild.building) {
-            lastBuildStatus = "BUILDING";
-        }
-        return lastBuildStatus;
-    }
-
-    buildJobs(jobs) {
-        let options = [];
-        jobs.forEach(job => {
-            options.push({
-                key: job.name,
-                value: job.name
-            })
-        });
-        return options;
-    }
-
-    parseApiKeyValue(apiKey){
-        if (apiKey == undefined) {
-            return;
-        }
-        
-        if (!apiKey.startsWith('http')) {
-            throw new Error('Unable to parse connection string. Connection needs to use protocol of "http" or "https".');
-        }
-
-        let urlParts = apiKey.split('//');
-        let protocol = urlParts[0];
-        if (!urlParts[1].includes('@')) {
-            throw new Error('Unable to parse connection string. Missing @ separator.');
-        }
-
-        let parts = urlParts[1].split('@');
-        this.jenkinsUrl = protocol + '//' + parts[1];
-        this.userToken = parts[0];
-    }
-    
+    const parts = urlParts[1].split('@');
+    this.jenkinsUrl = `${protocol}//${parts[1]}`;
+    this.userToken = parts[0].toString();
+  }
 }
 
 module.exports = {
-    JenkinsPipelineChecker: JenkinsPipelineChecker
-}
+  JenkinsPipelineChecker,
+};
 
+// eslint-disable-next-line no-unused-vars
 const applet = new JenkinsPipelineChecker();
--- test/JenkinsPipelineChecker.spec.js
+++ test/JenkinsPipelineChecker.spec.js
@@ -1,192 +1,188 @@
+/* eslint-disable no-undef */
 const assert = require('assert');
 const { JenkinsPipelineChecker } = require('../index');
 
 const testConfiguration = {
-	authorization: {
-		apiKey: 'http://1:1@host'
-	},
-	applet: {
-		user: {
-			pipeline: 'pipeline',
-			successColor: '#00FF00',
-			successEffect: 'SET_COLOR',
-			failureColor: '#FF0000',
-			failureEffect: 'SET_COLOR',
-			buildingColor: '#0000FF',
-			buildingEffect: 'SET_COLOR',
-			abortedColor: '#C0C0C0',
-			abortedEffect: 'SET_COLOR'
-		}
-	}
+  authorization: {
+    apiKey: 'http://1:1@host',
+  },
+  applet: {
+    user: {
+      pipeline: 'pipeline',
+      successColor: '#00FF00',
+      successEffect: 'SET_COLOR',
+      failureColor: '#FF0000',
+      failureEffect: 'SET_COLOR',
+      buildingColor: '#0000FF',
+      buildingEffect: 'SET_COLOR',
+      abortedColor: '#C0C0C0',
+      abortedEffect: 'SET_COLOR',
+    },
+  },
 };
 
-describe('JenkinsPipelineChecker', function () {
-    let jenkinsApiResponse = { building: false, result: 'SUCCESS', url: 'url to pipeline' };
-    let sut;
-    describe('the run() method', () => {
-        this.beforeAll(function () {
-            sut = buildApp(jenkinsApiResponse);
-        });
-
-        it('should return the appropriate signal color for a successful pipeline', async () => {
-            return sut.run().then((signal) => {
-                assert.equal(signal.points[0][0].color, testConfiguration.applet.user.successColor, 'Did not get the expected successful color.');
-            });
-        });
-
-        it('should return the appropriate signal effect for a successful pipeline', async () => {
-            return sut.run().then((signal) => {
-                assert.equal(signal.points[0][0].effect, testConfiguration.applet.user.successEffect, 'Did not get the expected successful effect.');
-            });
-        });
-
-        it('should return the appropriate signal message for a successful pipeline', async () => {
-            return sut.run().then((signal) => {
-                assert.ok(signal.message.includes('passed'), 'Message did not indicate if the pipeline passed');
-                assert.ok(signal.message.includes(testConfiguration.applet.user.pipeline), 'Message did not include pipeline name.');
-            });
-        });
-
-        it('should return the appropriate signal color for a failed pipeline', async () => {
-            jenkinsApiResponse.result = 'FAILURE';
-            return sut.run().then((signal) => {
-                assert.equal(signal.points[0][0].color, testConfiguration.applet.user.failureColor, 'Did not get the expected failure color.');
-            });
-        });
-
-        it('should return the appropriate signal effect for a failed pipeline', async () => {
-            jenkinsApiResponse.result = 'FAILURE';
-            return sut.run().then((signal) => {
-                assert.equal(signal.points[0][0].effect, testConfiguration.applet.user.failureEffect, 'Did not get the expected failure effect.');
-            });
-        });
-
-        it('should return the appropriate signal message for a failed pipeline', async () => {
-            jenkinsApiResponse.result = 'FAILURE';
-            return sut.run().then((signal) => {
-                assert.ok(signal.message.includes('failed'), 'Message did not indicate if the pipeline failed');
-                assert.ok(signal.message.includes(testConfiguration.applet.user.pipeline), 'Message did not include pipeline name.');
-            });
-        });
-
-        it('should return the appropriate signal color for a aborted pipeline', async () => {
-            jenkinsApiResponse.result = 'ABORTED';
-            return sut.run().then((signal) => {
-                assert.equal(signal.points[0][0].color, testConfiguration.applet.user.abortedColor, 'Did not get the expected aborted color.');
-            });
-        });
-
-        it('should return the appropriate signal effect for a aborted pipeline', async () => {
-            jenkinsApiResponse.result = 'ABORTED';
-            return sut.run().then((signal) => {
-                assert.equal(signal.points[0][0].effect, testConfiguration.applet.user.abortedEffect, 'Did not get the expected aborted effect.');
-            });
-        });
-
-        it('should return the appropriate signal message for a aborted pipeline', async () => {
-            jenkinsApiResponse.result = 'ABORTED';
-            return sut.run().then((signal) => {
-                assert.ok(signal.message.includes('aborted'), 'Message did not indicate if the pipeline aborted');
-                assert.ok(signal.message.includes(testConfiguration.applet.user.pipeline), 'Message did not include pipeline name.');
-            });
-        });
-
-        it('should return the appropriate signal color for a building pipeline', async () => {
-            jenkinsApiResponse.building = true;
-            jenkinsApiResponse.result = null;
-            return sut.run().then((signal) => {
-                assert.equal(signal.points[0][0].color, testConfiguration.applet.user.buildingColor, 'Did not get the expected building color.');
-            });
-        });
-
-        it('should return the appropriate signal effect for a building pipeline', async () => {
-            jenkinsApiResponse.building = true;
-            jenkinsApiResponse.result = null;
-            return sut.run().then((signal) => {
-                assert.equal(signal.points[0][0].effect, testConfiguration.applet.user.failureEffect, 'Did not get the expected building effect.');
-            });
-        });
-
-        it('should return the appropriate signal message for a building pipeline', async () => {
-            jenkinsApiResponse.building = true;
-            jenkinsApiResponse.result = null;
-            return sut.run().then((signal) => {
-                assert.ok(signal.message.includes('building'), 'Message did not indicate if the pipeline is building');
-                assert.ok(signal.message.includes(testConfiguration.applet.user.pipeline), 'Message did not include pipeline name.');
-            });
-        });
-
-        it('should return the appropriate signal link for a pipeline', async () => {
-            return sut.run().then((signal) => {
-                assert.equal(signal.link.url, 'url to pipeline', 'Did not get the expected successful effect.');
-            });
-        });
-
-        it('should properly parse user token from a valid key value', () => {
-            assert.equal(sut.userToken, '1:1', 'User token ws not properly parsed.');
-        });
-
-        it('should properly parse server url from a valid key value', () => {
-            assert.equal(sut.jenkinsUrl, 'http://host', 'Server url was not properly parsed.');
-        });
+function buildResponsePromise(response, rejection) {
+  let responsePromise;
+  if (response) {
+    responsePromise = new Promise((resolve, reject) => {
+      resolve(response);
     });
-
-    describe('the parseApiKeyValue() method', () => {
-        let jenkinsApiResponse = { building: false, result: 'SUCCESS', url: 'url to pipeline' };
-        let sut;
-
-        this.beforeAll(function() {
-            sut = buildApp(jenkinsApiResponse);
-        });
-
-        it ('properly throws exception when unable to detect proper protocol', () => {
-            assert.throws(() => {
-                sut.parseApiKeyValue('user:token@host')
-            },
-            {
-                message: /^Unable to parse connection string.*protocol.*$/
-            });
-        });
-
-        it ('properly throws exception when unable to detect token and host separator, @', () => {
-            assert.throws(() => {
-                sut.parseApiKeyValue('http://user:tokenhost')
-            },
-            {
-                message: /^Unable to parse connection string.*@.*$/
-            });
-        });
-
-        it ('should not perform parsing when passed undefined', () => {
-            sut.parseApiKeyValue(undefined);
-            assert.equal(sut.jenkinsUrl, 'http://host', 'Url was unexpectedly adjusted');
-            assert.equal(sut.userToken, '1:1', 'Token was unexpectedly adjusted');
-        });
-
+  } else {
+    responsePromise = new Promise((resolve, reject) => {
+      reject(rejection);
     });
-});
+  }
+  return responsePromise;
+}
 
 function buildApp(response, rejection) {
-    let fakeRequest = { get:  function() {
-        return buildResponsePromise(response, rejection);
-    }}
-    let app = new JenkinsPipelineChecker(fakeRequest);
-    app.processConfig(testConfiguration);
-    app.applyConfig();
+  const fakeRequest = {
+    get() {
+      return buildResponsePromise(response, rejection);
+    },
+  };
+  const app = new JenkinsPipelineChecker(fakeRequest);
+  app.processConfig(testConfiguration);
+  app.applyConfig();
 
-    return app;
+  return app;
 }
 
-function buildResponsePromise(response, rejection){
-    let responsePromise;
-    if (response) {
-        responsePromise = new Promise((resolve, reject) => {
-            resolve(response);
-        });
-    } else {
-        responsePromise = new Promise((resolve, reject) => {
-            reject(rejection);
-        });
-    }
-    return responsePromise;
-}
+
+describe('JenkinsPipelineChecker', () => {
+  describe('the run() method', () => {
+    const jenkinsApiResponse = { building: false, result: 'SUCCESS', url: 'url to pipeline' };
+    let sut;
+
+    before(() => {
+      sut = buildApp(jenkinsApiResponse);
+    });
+
+    it('should return the appropriate signal color for a successful pipeline', async () => sut.run().then((signal) => {
+      assert.equal(signal.points[0][0].color, testConfiguration.applet.user.successColor, 'Did not get the expected successful color.');
+    }));
+
+    it('should return the appropriate signal effect for a successful pipeline', async () => sut.run().then((signal) => {
+      assert.equal(signal.points[0][0].effect, testConfiguration.applet.user.successEffect, 'Did not get the expected successful effect.');
+    }));
+
+    it('should return the appropriate signal message for a successful pipeline', async () => sut.run().then((signal) => {
+      assert.ok(signal.message.includes('passed'), 'Message did not indicate if the pipeline passed');
+      assert.ok(signal.message.includes(testConfiguration.applet.user.pipeline), 'Message did not include pipeline name.');
+    }));
+
+    it('should return the appropriate signal color for a failed pipeline', async () => {
+      jenkinsApiResponse.result = 'FAILURE';
+      return sut.run().then((signal) => {
+        assert.equal(signal.points[0][0].color, testConfiguration.applet.user.failureColor, 'Did not get the expected failure color.');
+      });
+    });
+
+    it('should return the appropriate signal effect for a failed pipeline', async () => {
+      jenkinsApiResponse.result = 'FAILURE';
+      return sut.run().then((signal) => {
+        assert.equal(signal.points[0][0].effect, testConfiguration.applet.user.failureEffect, 'Did not get the expected failure effect.');
+      });
+    });
+
+    it('should return the appropriate signal message for a failed pipeline', async () => {
+      jenkinsApiResponse.result = 'FAILURE';
+      return sut.run().then((signal) => {
+        assert.ok(signal.message.includes('failed'), 'Message did not indicate if the pipeline failed');
+        assert.ok(signal.message.includes(testConfiguration.applet.user.pipeline), 'Message did not include pipeline name.');
+      });
+    });
+
+    it('should return the appropriate signal color for a aborted pipeline', async () => {
+      jenkinsApiResponse.result = 'ABORTED';
+      return sut.run().then((signal) => {
+        assert.equal(signal.points[0][0].color, testConfiguration.applet.user.abortedColor, 'Did not get the expected aborted color.');
+      });
+    });
+
+    it('should return the appropriate signal effect for a aborted pipeline', async () => {
+      jenkinsApiResponse.result = 'ABORTED';
+      return sut.run().then((signal) => {
+        assert.equal(signal.points[0][0].effect, testConfiguration.applet.user.abortedEffect, 'Did not get the expected aborted effect.');
+      });
+    });
+
+    it('should return the appropriate signal message for a aborted pipeline', async () => {
+      jenkinsApiResponse.result = 'ABORTED';
+      return sut.run().then((signal) => {
+        assert.ok(signal.message.includes('aborted'), 'Message did not indicate if the pipeline aborted');
+        assert.ok(signal.message.includes(testConfiguration.applet.user.pipeline), 'Message did not include pipeline name.');
+      });
+    });
+
+    it('should return the appropriate signal color for a building pipeline', async () => {
+      jenkinsApiResponse.building = true;
+      jenkinsApiResponse.result = null;
+      return sut.run().then((signal) => {
+        assert.equal(signal.points[0][0].color, testConfiguration.applet.user.buildingColor, 'Did not get the expected building color.');
+      });
+    });
+
+    it('should return the appropriate signal effect for a building pipeline', async () => {
+      jenkinsApiResponse.building = true;
+      jenkinsApiResponse.result = null;
+      return sut.run().then((signal) => {
+        assert.equal(signal.points[0][0].effect, testConfiguration.applet.user.failureEffect, 'Did not get the expected building effect.');
+      });
+    });
+
+    it('should return the appropriate signal message for a building pipeline', async () => {
+      jenkinsApiResponse.building = true;
+      jenkinsApiResponse.result = null;
+      return sut.run().then((signal) => {
+        assert.ok(signal.message.includes('building'), 'Message did not indicate if the pipeline is building');
+        assert.ok(signal.message.includes(testConfiguration.applet.user.pipeline), 'Message did not include pipeline name.');
+      });
+    });
+
+    it('should return the appropriate signal link for a pipeline', async () => sut.run().then((signal) => {
+      assert.equal(signal.link.url, 'url to pipeline', 'Did not get the expected successful effect.');
+    }));
+
+    it('should properly parse user token from a valid key value', () => {
+      assert.equal(sut.userToken, '1:1', 'User token ws not properly parsed.');
+    });
+
+    it('should properly parse server url from a valid key value', () => {
+      assert.equal(sut.jenkinsUrl, 'http://host', 'Server url was not properly parsed.');
+    });
+  });
+
+  describe('the parseApiKeyValue() method', () => {
+    const jenkinsApiResponse = { building: false, result: 'SUCCESS', url: 'url to pipeline' };
+    let sut;
+
+    before(() => {
+      sut = buildApp(jenkinsApiResponse);
+    });
+
+    it('properly throws exception when unable to detect proper protocol', () => {
+      assert.throws(() => {
+        sut.parseApiKeyValue('user:token@host');
+      },
+      {
+        message: /^Unable to parse connection string.*protocol.*$/,
+      });
+    });
+
+    it('properly throws exception when unable to detect token and host separator, @', () => {
+      assert.throws(() => {
+        sut.parseApiKeyValue('http://user:tokenhost');
+      },
+      {
+        message: /^Unable to parse connection string.*@.*$/,
+      });
+    });
+
+    it('should not perform parsing when passed undefined', () => {
+      sut.parseApiKeyValue(undefined);
+      assert.equal(sut.jenkinsUrl, 'http://host', 'Url was unexpectedly adjusted');
+      assert.equal(sut.userToken, '1:1', 'Token was unexpectedly adjusted');
+    });
+  });
+});
