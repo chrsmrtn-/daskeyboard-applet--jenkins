d4013d00a03a265afd59b0d4f9dccb7d17eb67e8
Chris Martin <chrsmrtn@outlook.com>
2020-03-03 21:30:13 -0700

    ensuring lf and addressed some lint errors

--- lastBuild.js
+++ lastBuild.js
@@ -1,15 +1,15 @@
 class LastBuild {
-    constructor(object) {
-        this._building = object.building;
-        this._result = object.result;
-        this._url = object.url;
-    }
+  constructor(object) {
+    this._building = object.building;
+    this._result = object.result;
+    this._url = object.url;
+  }
 
-    get building() { return this._building; }
+  get building() { return this._building; }
 
-    get result() {return this._result; }
+  get result() { return this._result; }
 
-    get url() { return this._url; }
+  get url() { return this._url; }
 }
 
 module.exports = { LastBuild };
